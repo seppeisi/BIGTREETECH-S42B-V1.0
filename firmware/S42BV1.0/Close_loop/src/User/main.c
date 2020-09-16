fb98213adbe1183edfd868aef26ccc5751f74e2a
seppeisi <52879686+seppeisi@users.noreply.github.com>
2020-09-16 15:53:54 +0200

    Update main.c

--- firmware/S42BV1.0/Close_loop/src/User/main.c
+++ firmware/S42BV1.0/Close_loop/src/User/main.c
@@ -505,7 +505,7 @@ int main(void){
   UART1_SendStr("MX_USART1_UART_Init OK\r\n");
   UART1_SendStr("Close Loop Model\r\n");  
   FlashUnlock();
-  Calibration_flag = FlashReadHalfWord(Data_Store_Arrdess);
+  Calibration_flag = FlashReadHalfWord(Data_Store_Arrdess);//0x8007C00
   FlashLock();
   if(Calibration_flag == 0xAA){
     UART1_SendStr("Calibration Ok\r\n");  
@@ -697,6 +697,67 @@ static void MX_SPI1_Init(void){
   LL_SPI_SetStandard(SPI1, LL_SPI_PROTOCOL_MOTOROLA); 
   LL_SPI_Enable(SPI1); 
 }
+
+static void MX_SPI2_Init(void){
+  LL_SPI_InitTypeDef SPI_InitStruct;
+  LL_GPIO_InitTypeDef GPIO_InitStruct;
+	
+  LL_APB1_GRP2_EnableClock(LL_APB1_GRP1_PERIPH_SPI2);
+
+  /*
+  LL_SPI_InitTypeDef SPI_InitStruct;
+  LL_GPIO_InitTypeDef GPIO_InitStruct;
+	
+  LL_APB1_GRP2_EnableClock(LL_APB1_GRP2_PERIPH_SPI1);
+  
+  /**SPI1 GPIO Configuration  
+  PA4   ------> SPI1_NSS
+  PA5   ------> SPI1_SCK
+  PA6   ------> SPI1_MISO
+  PA7   ------> SPI1_MOSI 
+  
+
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_5;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_DOWN;
+  GPIO_InitStruct.Alternate = LL_GPIO_AF_0;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_6;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_DOWN;
+  GPIO_InitStruct.Alternate = LL_GPIO_AF_0;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  GPIO_InitStruct.Pin = LL_GPIO_PIN_7;
+  GPIO_InitStruct.Mode = LL_GPIO_MODE_ALTERNATE;
+  GPIO_InitStruct.Speed = LL_GPIO_SPEED_FREQ_HIGH;
+  GPIO_InitStruct.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
+  GPIO_InitStruct.Pull = LL_GPIO_PULL_DOWN;
+  GPIO_InitStruct.Alternate = LL_GPIO_AF_0;
+  LL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  SPI_InitStruct.TransferDirection = LL_SPI_FULL_DUPLEX;
+  SPI_InitStruct.Mode = LL_SPI_MODE_MASTER;
+  SPI_InitStruct.DataWidth = LL_SPI_DATAWIDTH_16BIT;
+  SPI_InitStruct.ClockPolarity = LL_SPI_POLARITY_LOW;
+  SPI_InitStruct.ClockPhase = LL_SPI_PHASE_2EDGE;
+  SPI_InitStruct.NSS = LL_SPI_NSS_SOFT;
+  SPI_InitStruct.BaudRate = LL_SPI_BAUDRATEPRESCALER_DIV8;
+  SPI_InitStruct.BitOrder = LL_SPI_MSB_FIRST;
+  SPI_InitStruct.CRCCalculation = LL_SPI_CRCCALCULATION_DISABLE;
+  SPI_InitStruct.CRCPoly = 7;
+  LL_SPI_Init(SPI1, &SPI_InitStruct);
+  LL_SPI_SetStandard(SPI1, LL_SPI_PROTOCOL_MOTOROLA); 
+  LL_SPI_Enable(SPI1); 
+  */
+}
+
+
 //
 static void MX_TIM1_Init(void){
   LL_TIM_InitTypeDef TIM_InitStruct = {0};
